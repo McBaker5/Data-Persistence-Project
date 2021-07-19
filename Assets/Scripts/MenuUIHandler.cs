ffc991fb8ed891669227b9aad19d64af1e6f7328
McBaker5 <jackson@bakerplace.org>
2021-07-19 17:28:14 -0500

    added menu functionality
    the buttons and input field in the menu now work to start the game, exit, and enter the player's name

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine.UI;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    [SerializeField] InputField input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeName()
+    {
+        string name = input.text;
+        Debug.Log(name);
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // Original code to quit unity player
+#endif
+    }
+}
